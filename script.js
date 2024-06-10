390495e72f66f0ef673d4644df4cc3bcff9c0c46
Matvii Mykhailichenko <103883982+matthewthefool@users.noreply.github.com>
2024-06-10 17:13:33 +0200

    .

--- script.js
+++ script.js
@@ -1,41 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-    loadResearchData();
-});
-
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        tabcontent[i].classList.remove("active");
     }
+
+    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(tabName).style.display = "block";
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).classList.add("active");
     evt.currentTarget.className += " active";
 }
 
-function loadResearchData() {
-    fetch('research.json')
-        .then(response => response.json())
-        .then(data => {
-            const container = document.querySelector('.research-container');
-            data.publications.forEach(pub => {
-                const pubDiv = document.createElement('div');
-                pubDiv.className = 'research-item';
-                const imageUrl = pub.image || 'icons/image_placeholder.png';
-                pubDiv.innerHTML = `
-                    <img src="${imageUrl}" alt="Research image"> <!-- Research image -->
-                    <div class="research-item-content">
-                        <h3>${pub.title}</h3>
-                        <p><strong>Authors:</strong> ${pub.authors}</p>
-                        <p>${pub.details}</p>
-                        <a href="${pub.link}" target="_blank">Read More</a>
-                    </div>
-                `;
-                container.appendChild(pubDiv);
-            });
-        })
-        .catch(error => console.error('Error loading research data:', error));
-}
+// Set default tab to open after DOM is fully loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("defaultOpen").click();
+});
